72b3bcedeb7341f7193d359542d1cc586bafc549
JuanIrache <yrache@gmail.com>
2020-09-15 23:27:43 +0200

    support framerate and video duraiton from videos processed with Fusion Studio

--- index.js
+++ index.js
@@ -51,7 +51,10 @@ module.exports = function (file, isBrowser = false, update) {
           timing.start.setMinutes(
             timing.start.getMinutes() + timing.start.getTimezoneOffset()
           );
-        } else if (videoData.tracks[i].type == 'video') {
+        } else if (
+          videoData.tracks[i].type == 'video' ||
+          videoData.tracks[i].name === 'VideoHandler'
+        ) {
           var vid = videoData.tracks[i];
           timing.videoDuration = vid.movie_duration / vid.movie_timescale;
           //Deduce framerate from video track
